fbd1c80b51446f8f1db66a519e022893a1896f02
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-09-17 18:37:10 +0200

    updated dependencies units path, released version 1.3 - 20240917 (SDK and Delphi 12.2 Athens update)

--- src/Pompach.dpr
+++ src/Pompach.dpr
@@ -4,18 +4,18 @@ uses
   System.StartUpCopy,
   FMX.Forms,
   fMain in 'fMain.pas' {fmrMain},
-  Olf.RTL.Params in '..\lib-externes\librairies\Olf.RTL.Params.pas',
   cBattery in 'cBattery.pas' {cadBattery: TFrame},
   uDMTypoDesTitres in '..\_PRIVE\src\uDMTypoDesTitres.pas' {dmTypoDesTitres: TDataModule},
   Gamolf.RTL.Scores in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Scores.pas',
-  uKeyboardSpecialKeyTest in '..\lib-externes\librairies\uKeyboardSpecialKeyTest.pas',
-  Olf.FMX.TextImageFrame in '..\lib-externes\librairies\Olf.FMX.TextImageFrame.pas' {OlfFMXTextImageFrame: TFrame},
   Gamolf.FMX.Joystick in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.FMX.Joystick.pas',
   Gamolf.RTL.Joystick.DirectInput.Win in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Joystick.DirectInput.Win.pas',
   Gamolf.RTL.Joystick.Mac in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Joystick.Mac.pas',
   Gamolf.RTL.Joystick in '..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Joystick.pas',
   iOSapi.GameController in '..\lib-externes\Delphi-Game-Engine\src\iOSapi.GameController.pas',
-  Macapi.GameController in '..\lib-externes\Delphi-Game-Engine\src\Macapi.GameController.pas';
+  Macapi.GameController in '..\lib-externes\Delphi-Game-Engine\src\Macapi.GameController.pas',
+  Olf.FMX.TextImageFrame in '..\lib-externes\librairies\src\Olf.FMX.TextImageFrame.pas' {OlfFMXTextImageFrame: TFrame},
+  Olf.RTL.Params in '..\lib-externes\librairies\src\Olf.RTL.Params.pas',
+  uKeyboardSpecialKeyTest in '..\lib-externes\librairies\src\uKeyboardSpecialKeyTest.pas';
 
 {$R *.res}
 
