e210d9bddbbeded63b9d40ef542798930ace4c4a
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-09-17 18:47:02 +0200

    added header copyright comments (#39)

--- src-dessin-logo/Unit1.pas
+++ src-dessin-logo/Unit1.pas
@@ -1,3 +1,35 @@
+/// <summary>
+/// ***************************************************************************
+///
+/// Pompach
+///
+/// Copyright 2017-2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://pompach.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Pompach/
+///
+/// ***************************************************************************
+/// File last update : 2024-09-17T17:59:24.480+02:00
+/// Signature : a3c3e3f4d33e65df524b1f4833dab71949a6fc64
+/// ***************************************************************************
+/// </summary>
+
 unit Unit1;
 
 interface
--- src/Pompach.dpr
+++ src/Pompach.dpr
@@ -1,3 +1,35 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// Pompach
+///
+/// Copyright 2017-2024 Patrick Prémartin under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://pompach.gamolf.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/Pompach/
+///
+/// ***************************************************************************
+/// File last update : 2024-09-17T18:04:14.000+02:00
+/// Signature : d60d99dfa2cc8327d8fa1dd04a39572919f1d28f
+/// ***************************************************************************
+/// </summary>
+
 program Pompach;
 
 uses
